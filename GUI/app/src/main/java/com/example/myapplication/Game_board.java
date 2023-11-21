941d87252fce3ef727d8fa585f199f0c015592c4
Max <mtwardy@campus.uni-paderborn.de>
2023-11-22 00:36:04 +0100

    Spielbrett_anfang
    verzweiflung

--- GUI/app/src/main/java/com/example/myapplication/Game_board.java
+++ GUI/app/src/main/java/com/example/myapplication/Game_board.java
@@ -2,24 +2,26 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
+
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
-
-import com.example.myapplication.databinding.FragmentFirstBinding;
-import com.example.myapplication.databinding.FragmentMatchHistoryBinding;
+//databinding
+import com.example.myapplication.databinding.FragmentGameBoardBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MatchHistory#newInstance} factory method to
+ * Use the {@link Game_board#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MatchHistory extends Fragment {
+public class Game_board extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,9 +32,9 @@ public class MatchHistory extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private FragmentMatchHistoryBinding binding;
+    private FragmentGameBoardBinding binding;
 
-    public MatchHistory() {
+    public Game_board() {
         // Required empty public constructor
     }
 
@@ -42,48 +44,52 @@ public class MatchHistory extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MatchHistory.
+     * @return A new instance of fragment Game_board.
      */
     // TODO: Rename and change types and number of parameters
-    public static MatchHistory newInstance(String param1, String param2) {
-        MatchHistory fragment = new MatchHistory();
+    public static Game_board newInstance(String param1, String param2) {
+        Game_board fragment = new Game_board();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+        //
+        System.out.println("Gameboard created");
         return fragment;
-
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //still trying something
+        ViewGroup.LayoutParams GB_layout = binding.gameBoardLayout.getLayoutParams();
+
+        //getting Display size in dp
+        DisplayMetrics displayMetrics  = getContext().getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+
+
+        System.out.println(displayMetrics.heightPixels);
+        System.out.println(dpHeight);
+        System.out.println(displayMetrics.widthPixels);
+        System.out.println(dpWidth);
+
+
+
         if (getArguments() != null) {
+            System.out.println("da hail this");
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-
-        binding = FragmentMatchHistoryBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        binding.Game00001.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(MatchHistory.this)
-                        .navigate(R.id.action_matchHistory_to_game_board);
-
-
-            }
-        });
-
+        return inflater.inflate(R.layout.fragment_game_board, container, false);
     }
 }
