09b5671797dba804dfd386d7e45103c6a406296c
Abderrahmane Tabbi <tabbi@mail.upb.de>
2023-11-26 01:24:32 +0100

    game config Gui und Ausrichter Lobby Gui

--- src/main/java/org/example/gameConfig.java
+++ src/main/java/org/example/gameConfig.java
@@ -5,28 +5,135 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class gameConfig {
-    private JButton startGameButton;
-    private JPanel panel1;
-    private JTextField playerCount;
-    private JTextField figureCount;
-    private JTextField fieldSize;
-    private JTextField drawcardCount;
-    private JTextField initialCardsPerPlayer;
-    private JTextField thinkTimePerMove;
-    private JTextField visualTime;
-    private JTextField maxTime;
-    private JRadioButton kickFromGameRB;
-    private JRadioButton kickFromRoundRB;
-    private JTextField maxMoves;
-    private JRadioButton randomRB;
-    private JRadioButton notRandomRB;
+
+
+        public JPanel panel1;
+        private JTextField playerCount;
+        private JTextField figureCount;
+        private JTextField fieldSize;
+        private JTextField drawcardCount;
+        private JTextField initialCardsPerPlayer;
+        private JTextField thinkTimePerMove;
+        private JTextField visualTime;
+        private JTextField maxTime;
+        private JRadioButton kickFromGameRB;
+        private JRadioButton kickFromRoundRB;
+        private JTextField maxMoves;
+        private JRadioButton randomRB;
+        private JRadioButton notRandomRB;
+        private JButton saveConfigButton;
+        private JButton initiateGameButton;
+        private JButton loadConficButton;
 
     public gameConfig() {
-        startGameButton.addActionListener(new ActionListener() {
+        ButtonGroup group1 = new ButtonGroup();
+        ButtonGroup group2 = new ButtonGroup();
+
+        // Add radio buttons to groups
+        group1.add(kickFromGameRB);
+        group1.add(kickFromRoundRB);
+        group2.add(randomRB);
+        group2.add(notRandomRB);
+
+        // Set initial selection
+        kickFromGameRB.setSelected(true);
+        randomRB.setSelected(true);
+
+        saveConfigButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {// reads the entered file and initiates a game with that config
+            public void actionPerformed(ActionEvent e) {
+                if (!validateAndEnableButtons()){
+                    return;
+                }
+                else{
+                    System.out.println("alles gut");
+                }
+                closeWindow();
 
             }
         });
+
+        initiateGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!validateAndEnableButtons()){
+                    return;
+                }
+                else{
+                    System.out.println("alles gut");
+                }
+            }
+        });
+
+
+    }
+
+    private boolean validateAndEnableButtons() {
+        try {
+            int playerCountValue = Integer.parseInt(playerCount.getText());
+            int figureCountValue = Integer.parseInt(figureCount.getText());
+            int fieldSizeValue = Integer.parseInt(fieldSize.getText());
+            int drawCardCountValue = Integer.parseInt(drawcardCount.getText());
+            int initialCardsPerPlayerValue = Integer.parseInt(initialCardsPerPlayer.getText());
+            int thinkTimePerMoveValue = Integer.parseInt(thinkTimePerMove.getText());
+            int visualTimeValue = Integer.parseInt(visualTime.getText());
+            int maxTimeValue = Integer.parseInt(maxTime.getText());
+            int maxMovesValue = Integer.parseInt(maxMoves.getText());
+
+            // Perform validation checks
+            if (playerCountValue < 2 || playerCountValue > 6) {
+                showError("Player count must be between 2 and 6.");
+                return false;
+            }
+
+            if (figureCountValue < 0) {
+                showError("Figure count must be a positive integer.");
+                return false;
+            }
+
+            if (fieldSizeValue < 0) {
+                showError("Field size must be a positive integer.");
+                return false;
+            }
+
+            if (drawCardCountValue < 0 || drawCardCountValue > fieldSizeValue) {
+                showError("Draw card count must be between 0 and field size.");
+                return false;
+            }
+
+            if (initialCardsPerPlayerValue <= 0 || initialCardsPerPlayerValue > 110 / playerCountValue) {
+                showError("Initial cards per player must be between 1 and " + (110 / playerCountValue) + ".");
+                return false;
+            }
+
+            if (thinkTimePerMoveValue < 0 || visualTimeValue < 0 || maxTimeValue < 0 || maxMovesValue < 0) {
+                showError("Time values and maximum moves must be non-negative integers.");
+                return false;
+            }
+            return true;
+
+            // If all validations pass, enable the buttons
+        } catch (NumberFormatException ex) {
+            showError("Please enter valid numerical values for the configuration.");
+            return false;
+        }
+    }
+
+    private void showError(String message) {
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+
+    }
+    private void closeWindow() {
+        // Find the parent JFrame and close it
+        SwingUtilities.getWindowAncestor(panel1).dispose();
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("gameConfig");
+        frame.setContentPane(new gameConfig().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/main/java/org/example/lobby.java
@@ -0,0 +1,44 @@
+package src.main.java.org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class lobby {
+    private JTable table1;
+    private JPanel panel1;
+    private JTable table2;
+    private JButton addGameButton;
+    private JButton startGameButton;
+    private JButton button3;
+    private JButton button4;
+    private JButton assignPlayerToGameButton;
+
+    public lobby() {
+        addGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openGameConfigWindow();
+            }
+        });
+
+        // Other initialization and logic for the lobby class
+    }
+
+    private void openGameConfigWindow() {
+        // Create and show the gameConfig window
+        JFrame gameConfigFrame = new JFrame("Game Configuration");
+        gameConfigFrame.setContentPane(new gameConfig().panel1);
+        gameConfigFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Close only this frame when the close button is clicked
+        gameConfigFrame.pack();
+        gameConfigFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lobby");
+        frame.setContentPane(new lobby().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
