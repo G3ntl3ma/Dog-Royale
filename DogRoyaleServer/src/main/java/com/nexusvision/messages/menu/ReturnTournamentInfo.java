52e41fa5ecac607ba36eb8281d456bbe43918e23
bkeller <kellerb@mail.upb.de>
2023-11-30 19:28:17 +0100

    update Variables

--- DogRoyaleServer/src/main/java/com/nexusvision/messages/menu/ReturnTournamentInfo.java
+++ DogRoyaleServer/src/main/java/com/nexusvision/messages/menu/ReturnTournamentInfo.java
@@ -3,6 +3,8 @@ package com.nexusvision.messages.menu;
 import com.nexusvision.messages.game.AbstractGameMessage;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * Server gibt angefragte Turnierinformationen
  *
@@ -11,4 +13,57 @@ import lombok.Data;
 @Data
 public class ReturnTournamentInfo extends AbstractMenuMessage {
     //TODO implement variables
+    private List<TournamentInfo> tournamentInfo;
+
+    @Data
+    public static class TournamentInfo{
+        private int tournamentId;
+        private List<CurrentGame> currentGame;
+        private List<UpcomingGames> upcomingGames;
+        private List<CompletedGames> completedGames;
+        private List<CurrentRankings> currentRankings;
+
+        @Data
+        public static class CurrentGame{
+            private int gameId;
+            private int playerCount;
+            private int maxPlayerCount;
+            private int currentRound;
+            private List<Player> players;
+
+            @Data
+            public static class Player{
+                private int clientId;
+                private String name;
+                private int points;
+            }
+        }
+
+        @Data
+        public static class UpcomingGames{
+            private int gameId;
+            private int startInGames;
+            private List<Player> players;
+
+            @Data
+            public static class Player{
+                private int clientId;
+                private String name;
+                private int points;
+            }
+        }
+
+        @Data
+        public static class CompletedGames{
+            private int gameId;
+            private int winnerPlayerId;
+        }
+
+        @Data
+        public static class CurrentRankings{
+            private int clientId;
+            private String name;
+            private int points;
+        }
+    }
 }
