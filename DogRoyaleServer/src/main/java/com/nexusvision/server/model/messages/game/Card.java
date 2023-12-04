cd15da9a4bfefef9e3487561b0cc2bac7a1652ad
felixwr <felixwr@mail.uni-paderborn.de>
2023-12-04 17:44:02 +0100

    felixwr: Improved enum, added getOrdinal and getType

--- DogRoyaleServer/src/main/java/com/nexusvision/server/model/messages/game/Card.java
+++ DogRoyaleServer/src/main/java/com/nexusvision/server/model/messages/game/Card.java
@@ -1,6 +1,8 @@
 package com.nexusvision.server.model.messages.game;
 
 /**
+ * Enum that specifies the card
+ *
  * @author felixwr
  */
 public enum Card {
@@ -18,5 +20,24 @@ public enum Card {
     oneToSeven,
     magnetCard,
     swapCard,
-    copyCard
+    copyCard;
+
+    /**
+     * Maps the correct ordinal value by taking care of the shift
+     *
+     * @return The correct ordinal value
+     */
+    public int getOrdinal() {
+        return ordinal();
+    }
+
+    /**
+     * Maps the correct type by taking care of the shift
+     *
+     * @param ordinal The ordinal that will be mapped to it's type
+     * @return The correct type
+     */
+    public static Card getType(int ordinal) {
+        return values()[ordinal];
+    }
 }
