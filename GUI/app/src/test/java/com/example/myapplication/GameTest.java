813f853771892398af8b3a63811cbe0d678fa95a
Bastian22742 <148690971+Bastian22742@users.noreply.github.com>
2023-12-09 18:45:48 +0100

    new test

---
+++ GUI/app/src/test/java/com/example/myapplication/GameTest.java
@@ -0,0 +1,72 @@
+package com.example.myapplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void getGameID() {
+    }
+
+    @Test
+    void setGameID() {
+    }
+
+    @Test
+    void getTimeSeconds() {
+    }
+
+    @Test
+    void setTimeSeconds() {
+    }
+
+    @Test
+    void getCurrentPlayers() {
+    }
+
+    @Test
+    void setCurrentPlayers() {
+    }
+
+    @Test
+    void getMaxPlayers() {
+    }
+
+    @Test
+    void setMaxPlayers() {
+    }
+
+    @Test
+    void getFieldSize() {
+    }
+
+    @Test
+    void setFieldSize() {
+    }
+
+    @Test
+    void getFigureCount() {
+    }
+
+    @Test
+    void setFigureCount() {
+    }
+
+    @Test
+    void getDrawFields() {
+    }
+
+    @Test
+    void setDrawFields() {
+    }
+
+    @Test
+    void getStartFields() {
+    }
+
+    @Test
+    void setStartFields() {
+    }
+}
