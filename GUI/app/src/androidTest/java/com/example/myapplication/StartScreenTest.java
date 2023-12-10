499b90f106038c9da03b850442fc39a32a6778ef
Bastian22742 <148690971+Bastian22742@users.noreply.github.com>
2023-12-10 15:50:28 +0100

    test updates

--- GUI/app/src/androidTest/java/com/example/myapplication/StartScreenTest.java
+++ GUI/app/src/androidTest/java/com/example/myapplication/StartScreenTest.java
@@ -1,7 +1,21 @@
 package com.example.myapplication;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.Test;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 class StartScreenTest {
-
+    @Test
+    FragmentScenario<StartScreen> fragmentScenario = FragmentScenario.launchInContainer(StartScreen.class);
+    Espresso.
 }
