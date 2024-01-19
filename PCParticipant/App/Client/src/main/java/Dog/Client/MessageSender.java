8964aa6a98b0e2feb1db7f5b9d6887d810823a9e
Abderrahmane Tabbi <tabbi@mail.upb.de>
2024-01-19 22:21:15 +0100

    Tabbi: MessageSender class

---
+++ PCParticipant/App/Client/src/main/java/Dog/Client/MessageSender.java
@@ -0,0 +1,24 @@
+package Dog.Client;
+
+import Dtos.*;
+import Enums.Card;
+import javafx.fxml.FXML;
+
+public class MessageSender {
+    @FXML
+    Client client;
+
+    public void sendLeavePlayer() {
+        client.sendMessage(new LeavePlayerDto().toJson());
+    }
+    public void sendJoinGameAsPlayer(int gameId,int clientId,String playerName){
+        client.sendMessage(new JoinGameAsParticipantDto(gameId, clientId, playerName).toJson());
+    }
+    public void sendRegisterForTournament(int tournamentId, int clientId){
+        client.sendMessage(new RegisterForTournamentDto(tournamentId, clientId).toJson());
+    }
+    public void sendMove (boolean skip, Card card, int selectedValue, int pieceId, boolean isStarter, int opponentPieceId){
+        client.sendMessage(new MoveDto(skip, card, selectedValue, pieceId, isStarter, opponentPieceId).toJson());
+    }
+
+}
