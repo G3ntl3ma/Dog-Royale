53ab0cab3b37a5a6491d4f7c13652a83e66361a7
G3ntl3ma <luca_eisen@web.de>
2024-01-28 20:38:03 +0100

    leisen: give Player Feedback when Selecting a Value

--- PCParticipant/App/Client/src/main/java/views/selectValueController.java
+++ PCParticipant/App/Client/src/main/java/views/selectValueController.java
@@ -16,6 +16,35 @@ public class selectValueController {
 
     private Stage stage;
 
+    @FXML
+    private Button startCard1Startbtn;
+    @FXML
+    private Button startCard1Thirteenbtn;
+    @FXML
+    private Button startCard2Startbtn;
+    @FXML
+    private Button startCard2Onebtn;
+    @FXML
+    private Button startCard2Elevenbtn;
+    @FXML
+    private Button plusMinus4Plusbtn;
+    @FXML
+    private Button plusMinus4Minusbtn;
+    @FXML
+    private Button oneToSeven1btn;
+    @FXML
+    private Button oneToSeven2btn;
+    @FXML
+    private Button oneToSeven3btn;
+    @FXML
+    private Button oneToSeven4btn;
+    @FXML
+    private Button oneToSeven5btn;
+    @FXML
+    private Button oneToSeven6btn;
+    @FXML
+    private Button oneToSeven7btn;
+
     /**
      * sets the selected value in a CardHandler to 1 and updates a boolean flag in the CardHandler
      * to indicate that the selected value is not a starter value
@@ -23,6 +52,24 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select1Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if(pressed.getId().equals("startCard2Onebtn")){
+            startCard2Onebtn.setDisable(true);
+            startCard2Startbtn.setDisable(false);
+            startCard2Elevenbtn.setDisable(false);
+        } else if (pressed.getId().equals("oneToSeven1btn")) {
+            oneToSeven1btn.setDisable(true);
+            oneToSeven2btn.setDisable(false);
+            oneToSeven3btn.setDisable(false);
+            oneToSeven4btn.setDisable(false);
+            oneToSeven5btn.setDisable(false);
+            oneToSeven6btn.setDisable(false);
+            oneToSeven7btn.setDisable(false);
+        }
+
+
         CardHandler.setSelectedValue(1);
         CardHandler.setIsStarter(false);
     }
@@ -34,6 +81,19 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select2Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if (pressed.getId().equals("oneToSeven2btn")) {
+            oneToSeven2btn.setDisable(true);
+            oneToSeven1btn.setDisable(false);
+            oneToSeven3btn.setDisable(false);
+            oneToSeven4btn.setDisable(false);
+            oneToSeven5btn.setDisable(false);
+            oneToSeven6btn.setDisable(false);
+            oneToSeven7btn.setDisable(false);
+        }
+
         CardHandler.setSelectedValue(2);
         CardHandler.setIsStarter(false);
     }
@@ -45,6 +105,18 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select3Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if (pressed.getId().equals("oneToSeven3btn")) {
+            oneToSeven3btn.setDisable(true);
+            oneToSeven1btn.setDisable(false);
+            oneToSeven2btn.setDisable(false);
+            oneToSeven4btn.setDisable(false);
+            oneToSeven5btn.setDisable(false);
+            oneToSeven6btn.setDisable(false);
+            oneToSeven7btn.setDisable(false);
+        }
         CardHandler.setSelectedValue(3);
         CardHandler.setIsStarter(false);
     }
@@ -56,6 +128,21 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select4Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if(pressed.getId().equals("plusMinus4Plusbtn")){
+            plusMinus4Plusbtn.setDisable(true);
+            plusMinus4Minusbtn.setDisable(false);
+        }else if (pressed.getId().equals("oneToSeven4btn")){
+            oneToSeven4btn.setDisable(true);
+            oneToSeven1btn.setDisable(false);
+            oneToSeven2btn.setDisable(false);
+            oneToSeven3btn.setDisable(false);
+            oneToSeven5btn.setDisable(false);
+            oneToSeven6btn.setDisable(false);
+            oneToSeven7btn.setDisable(false);
+        }
         CardHandler.setSelectedValue(4);
         CardHandler.setIsStarter(false);
     }
@@ -67,6 +154,18 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select5Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if (pressed.getId().equals("oneToSeven5btn")) {
+            oneToSeven5btn.setDisable(true);
+            oneToSeven1btn.setDisable(false);
+            oneToSeven2btn.setDisable(false);
+            oneToSeven3btn.setDisable(false);
+            oneToSeven4btn.setDisable(false);
+            oneToSeven6btn.setDisable(false);
+            oneToSeven7btn.setDisable(false);
+        }
         CardHandler.setSelectedValue(5);
         CardHandler.setIsStarter(false);
     }
@@ -78,6 +177,18 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select6Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if (pressed.getId().equals("oneToSeven6btn")) {
+            oneToSeven6btn.setDisable(true);
+            oneToSeven3btn.setDisable(false);
+            oneToSeven1btn.setDisable(false);
+            oneToSeven2btn.setDisable(false);
+            oneToSeven4btn.setDisable(false);
+            oneToSeven5btn.setDisable(false);
+            oneToSeven7btn.setDisable(false);
+        }
         CardHandler.setSelectedValue(6);
         CardHandler.setIsStarter(false);
     }
@@ -89,6 +200,18 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select7Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if (pressed.getId().equals("oneToSeven7btn")) {
+            oneToSeven7btn.setDisable(true);
+            oneToSeven1btn.setDisable(false);
+            oneToSeven2btn.setDisable(false);
+            oneToSeven3btn.setDisable(false);
+            oneToSeven4btn.setDisable(false);
+            oneToSeven5btn.setDisable(false);
+            oneToSeven6btn.setDisable(false);
+        }
         CardHandler.setSelectedValue(7);
         CardHandler.setIsStarter(false);
     }
@@ -100,6 +223,13 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void selectMinus4Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if(pressed.getId().equals("plusMinus4Minusbtn")){
+            plusMinus4Minusbtn.setDisable(true);
+            plusMinus4Plusbtn.setDisable(false);
+        }
         CardHandler.setSelectedValue(-4);
         CardHandler.setIsStarter(false);
     }
@@ -110,6 +240,20 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void selectStartEvent(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if(pressed.getId().equals("startCard1Startbtn")){
+            startCard1Startbtn.setDisable(true);
+            startCard1Thirteenbtn.setDisable(false);
+        }
+        else if(pressed.getId().equals("startCard2Startbtn")){
+            startCard2Startbtn.setDisable(true);
+            startCard2Onebtn.setDisable(false);
+            startCard2Elevenbtn.setDisable(false);
+        }
+
+
         if (PieceImages.currentPiece != null) {
             PieceImages.currentPiece.deselect();
         }
@@ -136,6 +280,14 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select11Event(ActionEvent e){
+
+        //Give the Player Feedback that a Value has been selected
+        Button pressed = (Button)e.getSource();
+        if(pressed.getId().equals("startCard2Elevenbtn")){
+            startCard2Elevenbtn.setDisable(true);
+            startCard2Onebtn.setDisable(false);
+            startCard2Startbtn.setDisable(false);
+        }
         CardHandler.setSelectedValue(11);
         CardHandler.setIsStarter(false);
     }
@@ -147,6 +299,11 @@ public class selectValueController {
      * @param e represents an ActionEvent
      */
     public void select13Event(ActionEvent e){
+        Button pressed = (Button)e.getSource();
+        if(pressed.getId().equals("startCard1Thirteenbtn")){
+            startCard1Startbtn.setDisable(false);
+            startCard1Thirteenbtn.setDisable(true);
+        }
         CardHandler.setSelectedValue(13);
         CardHandler.setIsStarter(false);
     }
